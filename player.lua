65767cfd428d11c6290c69679c0786c1344344d6
s_lfurica <161180028+92q6@users.noreply.github.com>
2025-10-08 11:00:59 +1300

    Update player.lua

--- player.lua
+++ player.lua
@@ -8,6 +8,11 @@ function player.load()
     player.hp = player.mhp
     player.exp = 0
     player.g = 0
+
+    player.s = 0
+    player.se = 0
+    player.x = 0
+    player.y = 0
 end
 
 function player.update(i, ii, iii, iv, v) -- i = hp amount changed, ii = exp gained, iii = g gained, iv = item gained, v = item eaten
