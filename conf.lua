bc86390501a60cd45baf693cd1f01659e2de8216
s_lfurica <161180028+92q6@users.noreply.github.com>
2025-10-17 13:31:21 +1300

    window rename

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.width = 640
     t.window.height = 480
     t.window.fullscreen = false
-    t.window.title = "quiet"
+    t.window.title = "quiet water"
 
     -- t.window.icon = "icon.png"
 end
